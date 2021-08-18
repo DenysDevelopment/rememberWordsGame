f12b9202db071e72f20adb617a57909f7433a761
DenysDevelopment <dinis_05@ukr.net>
2021-08-18 20:23:14 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+const btns = document.querySelectorAll('.btn')
+const screens = document.querySelectorAll('.screen')
+let screen = 1
+
+for (let i = 0; i < btns.length; i++) {
+  const btn = btns[i];
+
+  btn.addEventListener('click', () => {
+    screen = +btn.dataset.screenNext;
+    screenShow(btn.dataset.screenNext - 1)
+    if (screen === 2) startGame()
+  })
+}
+
+function screensHide() {
+  for (let i = 0; i < screens.length; i++) {
+    screens[i].classList.add('screen--lock')
+  }
+}
+function screenShow(screen) {
+  screensHide()
+  screens[screen].classList.remove('screen--lock')
+}
+
+
+//========================================================================================================================================================
+// back-end
+
+const settings = {
+  words: 5,
+  mode: 1,
+  wordsPerSecond: 5
+}
+
+let currentWord = 1
+const defaultWords = ['Корова', "Авель", "Авелька"];
+
+
+function startGame() {
+  countDown(settings.wordsPerSecond)
+  showWordNext()
+  currentWord++
+  const nextWordTimer = setInterval(() => {
+    showWordNext()
+    currentWord++
+    if (currentWord > defaultWords.length) {
+      clearInterval(nextWordTimer)
+    }
+  }, (settings.wordsPerSecond + 1) * 1000)
+
+
+}
+
+function showWordNext(words) {
+  document.querySelector('.two-screen__text').innerHTML = defaultWords[currentWord - 1]
+  countDown(settings.wordsPerSecond)
+}
+
+function countDown(timeCount) {
+  let time = +timeCount
+  document.querySelector('.timer').innerHTML = time
+  --time
+  const timer = setInterval(() => {
+    if (time < 0) {
+      clearInterval(timer)
+    } else {
+      document.querySelector('.timer').innerHTML = time
+      --time
+    }
+  }, 1000)
+}
